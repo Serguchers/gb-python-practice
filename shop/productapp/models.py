f425b9f079dec6b9d90b8f1c29e6ed563a9cbab9
Sergei <serguchers@gmail.com>
2022-06-09 02:00:16 +0300

    product category

--- shop/productapp/models.py
+++ shop/productapp/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name="имя", max_length=64, unique=True)
+    description = models.TextField(verbose_name="описание", blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    
 class Product(models.Model):
+    category = models.ManyToManyField(ProductCategory, default='')
     product_name = models.CharField(max_length=128)
     date_in = models.DateField()
     price = models.IntegerField()
--- shop/productapp/views.py
+++ shop/productapp/views.py
@@ -1,7 +1,7 @@
-from asyncio import QueueEmpty
+from math import prod
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Product
+from .models import Product, ProductCategory
 # Create your views here.
 
 class ProductListView(ListView):
@@ -9,3 +9,14 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     print(queryset)
     template_name = 'products_list.html'
+    
+
+def products(request, pk=None):
+    
+    if pk is None:
+        products = Product.objects.all().prefetch_related('category')
+    else:
+        products = Product.objects.prefetch_related('category').filter(category__pk=pk).order_by('price')
+    
+    return render(request, 'products_list.html', {'object_list': products,
+                                                  'categories': ProductCategory.objects.all()})
