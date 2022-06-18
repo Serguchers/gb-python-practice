659dc38ecd71edf897cf4d7e42d21ae31d175c46
root <serguchers@gmail.com>
2022-06-18 15:34:46 +0200

    docker

---
+++ shop/shop/settings/debug.py
@@ -0,0 +1,19 @@
+from re import I
+from .base_settings import *
+
+SECRET_KEY = 'django-insecure-ps0qq@9+-sg8cd*y+*z#v_l47!ng$y=d1mup6%kx9!x8s%i8mb'
+
+DEBUG = True
+
+SITE_ID = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'sergei',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- shop/shop/settings/prod.py
+++ shop/shop/settings/prod.py
@@ -3,10 +3,10 @@ import configparser
 from .base_settings import *
 
 
-# SECRET_KEY = 'django-insecure-ps0qq@9+-sg8cd*y+*z#v_l47!ng$y=d1mup6%kx9!x8s%i8mb'
-config = configparser.ConfigParser()
-config_path = f'{os.getcwd()}/shop/settings/config.ini'
-config.read(config_path)
-SECRET_KEY = config['SETTINGS']['SECRET_KEY']
+SECRET_KEY = 'django-insecure-ps0qq@9+-sg8cd*y+*z#v_l47!ng$y=d1mup6%kx9!x8s%i8mb'
+# config = configparser.ConfigParser()
+# config_path = f'{os.getcwd()}/shop/settings/config.ini'
+# config.read(config_path)
+# SECRET_KEY = config['SETTINGS']['SECRET_KEY']
 
 DEBUG = False
